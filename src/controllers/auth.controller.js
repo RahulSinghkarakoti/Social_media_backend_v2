90cd2455dbf87eae91b462d881f9a15d9fe22332
rahul karakoti <rahulsinghkarakoti28@gmail.com>
2025-01-21 23:46:13 +0530

    minor changes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -35,6 +35,9 @@ const signup = asyncHandler(async (req, res) => {
   if (!username || !email || !password)
     return res.status(400).json({ message: "All fields are required" });
 
+  if(password.length < 6)
+    return res.status(400).json({ message: "Password must be at least 6 characters" });
+
  try {
    const existingUser = await User.findOne({
      $or: [{ email }, { username }],
